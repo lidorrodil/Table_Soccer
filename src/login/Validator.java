a254479582d40faf927cb3d5b6fa1ec51db44804
Fischer Joel (s) <joel.fischer@students.fhnw.ch>
2017-05-20 13:59:42 +0200

    solved merge conflict

--- src/login/Validator.java
+++ src/login/Validator.java
@@ -9,23 +9,30 @@ import javax.swing.plaf.synth.SynthSpinnerUI;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import tournament.TournamentModel;
+import tournament.TournamentView;
 import userInterface.MVC;
 
 public class Validator extends Application   {
     private Validator_View view;
     private Validator_Controller controller;
     private Validator_Model model;
+    private TournamentView tView;
+    private TournamentModel tModel;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         model = new Validator_Model();
         view = new Validator_View(primaryStage, model);
+        
+        tModel = new TournamentModel();
+        tView = new TournamentView(primaryStage, tModel);
         controller = new Validator_Controller(model, view);
         boolean flag;
         // Display the GUI after all initialization is complete
         
         view.start(); 
-
+        tView.startTournament();
        
     }
     
